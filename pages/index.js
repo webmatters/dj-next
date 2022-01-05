ba6a36219ff9ff1c78886e828da15cab10871638
webmatters <bodhicoder@gmail.com>
2022-01-04 20:13:50 -0500

    Finishes basic pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-import Layout from '../components/Layout'
+import Link from 'next/link'
 
-export default function HomePage() {
+import { API_URL } from '@/config/index'
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  }
 }
